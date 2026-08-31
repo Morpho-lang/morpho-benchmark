396a2dbefb6a669301b782a58dccec5fba217ed5
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2026-08-31 17:06:19 -0400

    Fix ising code

--- other/Ising/ising.py
+++ other/Ising/ising.py
@@ -1,81 +1,73 @@
-import numpy as np
-import random as rnd
-import time
+# Monte Carlo simulation of the 2D Ising model.
+
 from math import exp
+import random as rnd
 
-N=20
-Niter=10000
-T=2
+N = 20
+Niter = 10000
+T = 2
 
-# Initialize the board
-def init():
-    if (rnd.random()<0.5):
-        return 1
-    return -1
-
-a = np.zeros((N, N))
-
+a = []
 for i in range(N):
+    row = []
     for j in range(N):
-        a[i,j]=init()
+        if rnd.random() < 0.5:
+            row.append(-1)
+        else:
+            row.append(1)
+    a.append(row)
 
-# Calculate the energy of lattice site (i,j)
+
 def energy(a, i, j):
-    il = i - 1 if i > 0 else N - 1
-    ir = i + 1 if i < N - 1 else 0
-    jl = j - 1 if j > 0 else N - 1
-    jr = j + 1 if j < N - 1 else 0
-    return -a[i, j] * (a[il, j] + a[ir, j] + a[i, jl] + a[i, jr])
+    il = i - 1
+    if il < 0:
+        il = N - 1
+    ir = i + 1
+    if ir > N - 1:
+        ir = 0
+    jl = j - 1
+    if jl < 0:
+        jl = N - 1
+    jr = j + 1
+    if jr > N - 1:
+        jr = 0
+    return -a[i][j] * (a[il][j] + a[ir][j] + a[i][jl] + a[i][jr])
 
-# Calculate the total energy of the system
-def total(a):
-    en=0
-    for i in range(N):
-        for j in range(N):
-            en+=a[i,j]
-    return en
 
-# Magnetization
 def magnetization(a):
-    m=0
+    m = 0
     for i in range(N):
         for j in range(N):
-            m+=a[i,j]
-    return m/(N*N)
+            m += a[i][j]
+    return m / (N * N)
 
-# Visualization
+
 def vis(a):
     for i in range(N):
-        str=""
+        line = ""
         for j in range(N):
-            if (a[i,j]<0):
-                str+="-"
+            if a[i][j] < 0:
+                line += "-"
             else:
-                str+="X"
-        print(str);
-    return
+                line += "X"
+        print(line)
+
 
 vis(a)
 
 print("Run:")
-
-start = time.perf_counter()
 
 for n in range(Niter):
     for k in range(N * N):
         i = rnd.randrange(N)
         j = rnd.randrange(N)
         old = energy(a, i, j)
-        a[i, j] = -a[i, j]
+        a[i][j] = -a[i][j]
         new = energy(a, i, j)
         if new >= old:
-            if exp((old - new) / T) < rnd.random():
-                a[i, j] = -a[i, j]
-
-end = time.perf_counter()
+            if exp(-(new - old) / T) < rnd.random():
+                a[i][j] = -a[i][j]
 
 vis(a)
 
-print("Magnetization:", magnetization(a));
-
-print("Time", end-start)
+print("Magnetization:", magnetization(a))
